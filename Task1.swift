4cdbce66bf88e5886a33e29e8232881453813b05
Alex7225 <oleksandr_grigor@ukr.net>
2024-08-09 00:33:28 -0600

    Update Task1.swift
    Changed

--- Task1.swift
+++ Task1.swift
@@ -1,63 +1,63 @@
 import UIKit
-// 1
-var stringa = ["apple", "banana", "cherry"]
+// 1  Create an array of string values containing the names of at least 5 different fruits.
+var strings = ["apple", "banana", "cherry"]
 
-//2
-stringa.append("kiwi")
-stringa.append("elderberry")
-stringa.insert("Orange", at: 0)
+//2  Add two more fruits to the array.
+strings.append("kiwi")
+strings.append("elderberry")
+strings.insert("Orange", at: 0)
 
-// 3
-if stringa.contains("kiwi"){
-    print("String contains that word")
-} else {print("No matches")}
+// 3  Check if the array contains the fruit "kiwi".
+if strings.contains("kiwi"){
+    print("String contains name of fruit KIWI")
+} else {print("No matches name of fruit KIWI in this string")}
 
-// 4
-var str1 = stringa
-var index = stringa.count
+// 4  Remove the second and second-to-last elements from the array.
+var str1 = strings
+var countOfElements = strings.count
 str1.remove(at: 1)
-str1.remove(at: index-2)
-str1.remove(at: index-3)
+str1.remove(at: countOfElements-2)
+str1.remove(at: countOfElements-3)
 print(str1)
 
-// 5    не дуже розумію як це працює, але воно працює ), в інтренеті найшов
+// 5 Find and print the longest fruit name, from the new array created in step 4.
 if let max = str1.max(by: {$1.count > $0.count}) {
     print(max)
 }
 
-// 6
-var upperStringa = stringa.description.uppercased()
-print(upperStringa)
+// 6  Convert all fruit names in the original array to uppercase.
+var upperstrings = strings.description.uppercased()
+print(upperstrings)
 
-// 7
-stringa.sort()
-print(stringa)
+// 7  Sort the original array alphabetically.
+strings.sort()
+print(strings)
 
-// 8
-var concotStr = ""
-for index1 in stringa{
-    concotStr = concotStr + "," + index1
-    }
+// 8  Concatenate all the fruit names from the original array into a single string, separated by commas.
+var concotStr = strings.reduce(into: "", { $0 += $1 })
+//for countOfElements1 in strings{
+//    concotStr = concotStr + "," + countOfElements1
+//    }
 print(concotStr)
 
-// 9
+// 9  Find the total number of characters in the concatenated string from step 8.
 print("Total munber of characters is = \(concotStr.count)")
 
-// 10.a
+// 10.a  Count and print the number of vowels (a, e, i, o, u) in the concatenated string.
 var vowelCount = 0
-for ch in concotStr {
+concotStr.forEach( { ch in
     switch ch {
-    case "a": vowelCount += 1
-    case "e": vowelCount += 1
-    case "i": vowelCount += 1
-    case "o": vowelCount += 1
-    case "u": vowelCount += 1
-    default:
-        vowelCount
+    case "a", "e", "i", "o", "u":  vowelCount += 1
+    default: return
     }
- 
-}
-print(vowelCount)
+})
+    print(vowelCount)
 
-// 10.b  - це не робить, але можна зробити по принципу попереднього світчом
-let replStr = concotStr.replacingOccurrences(of: "a","e","i","o","u", with:"*" <#T##StringProtocol#>)
+
+// 10.b  Replace all vowels in the concatenated string with the character '*'.
+var replStr = concotStr.replacingOccurrences(of: "a", with:"*")
+replStr = replStr.replacingOccurrences(of: "e", with:"*")
+replStr = replStr.replacingOccurrences(of: "i", with:"*")
+replStr = replStr.replacingOccurrences(of: "o", with:"*")
+replStr = replStr.replacingOccurrences(of: "u", with:"*")
+print(replStr)
